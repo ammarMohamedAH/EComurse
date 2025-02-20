2fbbcae32266ccb406f32476ba8383de7cb997c5
ammarMohamedAH <ammarmohamedah@gmail.com>
2025-02-20 06:11:46 +0200

    first commit

--- src/Hooks/usePayment.jsx
+++ src/Hooks/usePayment.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 let token = localStorage.getItem("token");
 
 export default function usePayment({cartId,shippingAddress}) {
-    return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:5173`,{shippingAddress},{headers:{
+    return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://e-comurse-git-main-ammarmohamed01210gmailcoms-projects.vercel.app/`,{shippingAddress},{headers:{
         token,
       }})
 }
