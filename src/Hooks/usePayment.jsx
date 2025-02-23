2d93ca1ff79c233e302090ff3d9d6bcf66fda332
ammarMohamedAH <ammarmohamedah@gmail.com>
2025-02-23 22:38:16 +0200

    first commit ��last

--- src/Hooks/usePayment.jsx
+++ src/Hooks/usePayment.jsx
@@ -5,7 +5,7 @@ async function processPayment({ cartId, shippingAddress }) {
 
   
     return await axios.post(
-      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:5173`,
+      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://e-comurse-git-main-ammarmohamed01210gmailcoms-projects.vercel.app/allorders`,
       { shippingAddress },
       { headers: { token:localStorage.getItem('token') } }
     );
